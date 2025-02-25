75410b9ed58edcce017fab5ad753cfa5c17d2d88
Thobla <111702269+Thobla@users.noreply.github.com>
2025-02-25 22:15:17 +0100

    (Feat) StarShip movement implemented

---
+++ game/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using Godot;
+using System;
+
+public partial class PlayerMovement : CharacterBody2D
+{
+	static float MAX_SPEED = 3000f;
+
+	private float acceleration = 10000f;
+	
+	private float breakMultiplyer = 2f;
+
+
+	private Node2D focusPoint;
+
+	private Vector2 facingVector;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		focusPoint = GetNode<Node2D>("FocusPoint");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		facingVector = GetGlobalPosition().DirectionTo(focusPoint.GlobalPosition).Normalized();
+		Move(delta);
+		Rotate(delta);	
+	}
+
+	private void Move(double delta)
+	{
+		if (Input.IsActionPressed("Thrust"))
+		{
+			GD.Print("Thrusting");
+
+			Velocity = Velocity.MoveToward(facingVector*MAX_SPEED, (float) delta * acceleration);
+		}
+	
+		MoveAndCollide(Velocity * (float) delta);
+	}
+
+	private void Rotate(double delta)
+	{
+		LookAt(GetGlobalMousePosition());
+	}
+}
